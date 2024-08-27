cf63cc3a03813adb6f58d7d5267ed432e2d972a6
Bryan Vivas <brayan3110x@gmail.com>
2024-08-27 00:06:39 -0500

    v2

--- modelo.py
+++ modelo.py
@@ -1,8 +1,11 @@
 import mysql.connector
+import json
+from datetime import datetime, date, time
 from mysql.connector import Error
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox , Image
+now = datetime.now()
 
 class conexion:
     
@@ -102,12 +105,139 @@ class Login:
                     messagebox.showerror("ERROR DE INGRESO", "usuario o contraseña incorrecto") 
                 self.Limpiar_login()
         
-   
+ 
+class modelo:
+    def __init__(self) -> None:
+         self._nombre = None
+         self._descripcion = None
+         self._categoria = None
+         self._precio = None
+         self._vendido = None
+         self._dia = None
+         self._hora = None
+         self._base = []
+         
+    @property
+    def nombre(self):
+        return self._nombre
+    @property
+    def descripcion(self):
+        return self._descripcion
+    @property
+    def categoria(self):
+        return self._categoria
+    @property
+    def precio(self):
+        return self._precio
+    @property
+    def vendido(self):
+        return self._vendido
+    @property
+    def dia(self):
+        return self._dia
+    @property
+    def hora(self):
+        return self._hora
+    @property
+    def base(self):
+        return self._base
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    @descripcion.setter
+    def descripcion(self, descripcion):
+        self._descripcion = descripcion
+    @categoria.setter
+    def categoria(self, categoria):
+        self._categoria = categoria
+    @precio.setter
+    def precio(self, precio):
+        self._precio = precio
+    @vendido.setter
+    def vendido(self, vendido):
+        self._vendido = vendido
+    @dia.setter
+    def dia(self, dia):
+        self._dia = dia
+    @hora.setter
+    def hora(self, hora):
+        self._hora = hora
+    @categoria.setter
+    def base(self, base):
+        self._base = base
+    
+         
+    def registrarProducto(nombre, descripcion, categoria, precio, vendido, dia, hora):
 
-
-
+            conexion1 = conexion.crearConexion()
+            cursor = conexion1.cursor()
+            cursor.execute(f"INSERT INTO productos (`nombre`, `descripcion`, `categoria`, `precio`, `vendido`, `dia`, `hora`) VALUES ('{nombre}','{descripcion}','{categoria}','{precio}','{vendido}','{dia}','{hora}')")
+            conexion1.commit()
+            print("Datos guardados")
+            
+    def consultarDatos():
+        conexion1 = conexion.crearConexion()
+        cursor = conexion1.cursor()
+        cursor.execute(f"SELECT * FROM productos")
+        consulta = cursor.fetchall()
+        return consulta
+    
+    def consultarCategoria(categoria):
+        conexion1 = conexion.crearConexion()
+        cursor = conexion1.cursor()
+        cursor.execute(f"SELECT * FROM productos WHERE categoria = '{categoria}'")
+        consulta = cursor.fetchall()
+        return consulta
+        
+        
+    #def generar_archivo():
+     #   datos2 = modelo.consultarDatos()
+      #  json_dato=json.dumps(datos2)
+       # print(json_dato)
+        #serializar a no json
+       # sin_jason = json.loads(json_dato)
+       # print(sin_jason)
+        
+        #serializar y escribir en un archivo
+        
+            
+    
+            
+    def generarInforme(dia):
+            with open(f'{dia}', 'w', encoding='utf8') as archivo:
+                
+                json.dumps(modelo._base, archivo)
+            print("informe generado")
+                
+    
 #verificar si el modulo ha sido ejecutado correctamente  
-if __name__ == '__main__':
-    ventana_login=Tk()
-    application=Login(ventana_login)
-    ventana_login.mainloop()
+#if __name__ == '__main__':
+#    ventana_login=Tk()
+#    application=Login(ventana_login)
+#    ventana_login.mainloop()
+#    fecha = now.date()
+#    print(fecha)
+dia1 = now.date()
+hora1 = now.time()
+modelo._base = modelo.consultarDatos()
+print(modelo._base)
+resultado = modelo.consultarCategoria('limpieza')
+print(resultado)
+#modelo.generarInforme(dia1)
+nombre1 = input("nombre")
+descripcion1 = input("descrip")
+categoria1 = input("categoria")
+precio1 = float(input("precio"))
+vendido1 = int(input("vendido"))
+
+
+modelo.nombre = nombre1
+modelo.descripcion = descripcion1
+modelo.categoria = categoria1
+modelo.precio = precio1
+modelo.vendido = vendido1
+modelo.dia = dia1
+modelo.hora = hora1
+
+modelo.registrarProducto(modelo.nombre, modelo.descripcion, modelo.categoria, modelo.precio, modelo.vendido, modelo.dia, modelo.hora)
